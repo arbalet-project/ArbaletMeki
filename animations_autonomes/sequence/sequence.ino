2195371f8f840f6897094b6a6c55cfa8094645a2
Yoan Mollard <yoan.mollard@inria.fr>
2018-01-26 12:52:48 +0100

    Tetris integration (untested)

--- animations_autonomes/sequence/sequence.ino
+++ animations_autonomes/sequence/sequence.ino
@@ -6,9 +6,11 @@
 
 Adafruit_WS2801 strip = Adafruit_WS2801(300);
 unsigned long DURATION_ANIMATION_MS = 30000;
-int NUM_ANIMATIONS = 3;
+int NUM_ANIMATIONS = 4;
 unsigned long last_animation_switch = 0;
 int current_animation = 0;
+boolean isGame = false;
+char sens_tableau = '3';
 
 
 /***************************************************************** OUTILS DE CONVERSION *******************************************************/
@@ -89,6 +91,41 @@ void fadeOut() {
     strip.show();
     delay(10);
   }
+}
+
+
+int calculer(int ligne, int colonne) {
+    switch(sens_tableau){
+    case '0' :
+        if(ligne % 2 == 0){
+            return ligne * 20 + (19 - colonne);
+        } else{
+            return ligne * 20 + colonne;
+        }
+        break;
+    case '1' :
+        if(ligne % 2 == 0){
+            return ligne * 20 + colonne;
+        } else{
+            return ligne * 20 + (19 - colonne);
+        }
+        break;
+    case '2' :
+        if(ligne % 2 == 0){
+            return (14 - ligne) * 20 +  colonne;
+        } else{
+            return (14 - ligne) * 20 + (19 - colonne);
+        }
+        break;
+    case '3' :
+        if(ligne % 2 == 0){
+            return (14 - ligne) * 20 + (19 - colonne);
+        } else{
+            return (14 -ligne) * 20 + colonne;
+        }
+        break;
+
+    }
 }
 
 /***************************************************************** ROUE DES COULEURS *******************************************************/
@@ -242,6 +279,469 @@ void destroyLineaire() {
   free(hue_expos);
 }
 
+
+/***************************************************************** TETRIS ************************************************************/
+
+//  Matrix Definition
+#define cols         20
+#define rows        15
+#define leftLimit    0
+#define rightLimit   7
+#define topLimit     0
+#define bottomLimit 14
+
+int currentMatrix[rows][cols];
+
+
+/************************* Bluetooth ************************************/
+String list[10];
+boolean commands[10] = {false, false, false, false, false, false, false, false, false, false};
+
+void setupBluetooth() {
+  Serial1.begin(9600);
+  Serial1.setTimeout(10);
+  
+  list[0] = "Right_u";
+  list[1] = "Left_u";
+  list[2] = "Up_u";
+  list[3] = "Down_u";
+  list[4] = "Btn1_u";
+  list[5] = "Btn2_u";
+  list[6] = "Btn3_u";
+  list[7] = "Btn4_u";
+  list[8] = "Select_u";
+  list[9] = "Start_u"; 
+}
+
+void resetBluetoothCommands() {
+   for(int i=0; i<10; ++i) {
+      commands[i] = false;
+  } 
+}
+
+void updateBluetoothCommands() {
+  String s = Serial1.readString();
+  if(s.length() > 0) {
+    //s[s.length()-1] = 0;  // delete \r
+    //s[s.length()-2] = 0;  // delete \n
+
+    for(int i=0; i<10; ++i) {
+      if(s.startsWith(list[i])) {
+        commands[i] = true;
+        return;
+      }
+    }
+  } 
+}
+
+/******************************* Tetris lui-meme ****************************/
+#include "tetris_letters.h"
+#include "tetris_pieces.h"
+
+byte (*pieces)[4][4];
+byte (*piecesGenerated)[4][4];
+
+byte currentPieceIndex;        // stores current piece index: from 0 to 18
+byte currentPieceRotatedIndex; // stores the index of the rotated version of the current piece
+byte currentPiece[4][4];       // stores the current piece matrix
+
+//the top left corner of the current piece is tracked
+int currentPieceRow;           // stores the row position of currentPiece[0][0]
+int currentPieceCol;           // stores the col position of currentPiece[0][0]
+int currentPieceMovedRow;      // stores the row position of currentPiece[0][0] after a certain move
+int currentPieceMovedCol;      // stores the col position of currentPiece[0][0] after a certain move
+
+byte numberOfLinesCleared;
+boolean disableMove;
+boolean pieceDropping;
+byte textColor;
+boolean gameRunning;
+boolean fastDrop;
+
+unsigned long lastButtonPressTime=millis();
+unsigned long time= millis();
+
+//  used in the main loop to calculate the time interval between moves
+unsigned long lastMoveTime=millis();
+unsigned long currentMoveTime=millis();
+
+unsigned long lastLandedTime=millis();
+unsigned long currentLandedTime=millis();
+
+void clearTrace() {
+  for (int i=0;i<4;i++) {
+    for (int j=0;j<4;j++) {
+      if ((currentPieceRow+j>-1 && currentPieceRow+j<rows) && (currentPieceCol+i>-1 && currentPieceCol+i<cols)) {
+        if (currentMatrix[currentPieceRow+j][currentPieceCol+i]>0) currentMatrix[currentPieceRow+j][currentPieceCol+i]=0;
+      }
+    }
+  }
+}
+
+void updateCurrentMatrix() {
+  for (int i=0;i<4;i++) {
+    for (int j=0;j<4;j++) {
+      if ((currentPieceRow+j>-1 && currentPieceRow+j<rows) && (currentPieceCol+i>-1 && currentPieceCol+i<cols)) {
+        if (currentPiece[j][i]!=0) currentMatrix[currentPieceRow+j][currentPieceCol+i]=currentPiece[j][i];
+      }
+    }
+  }
+}
+
+void move(int moveDirection) {
+  if (moveDirection==2) {
+    currentPieceMovedRow=currentPieceRow+1;
+    currentPieceMovedCol=currentPieceCol;
+  }
+  else if (moveDirection==3) {
+    currentPieceMovedRow=currentPieceRow;
+    currentPieceMovedCol=currentPieceCol-1;
+  }
+  else if (moveDirection==4) {
+    currentPieceMovedRow=currentPieceRow;
+    currentPieceMovedCol=currentPieceCol+1;
+  }
+  // check if move is valid
+  if (!checkOverlapAndOutOfBoundary(currentPieceIndex)) {
+    clearTrace();
+    currentPieceRow=currentPieceMovedRow;
+    currentPieceCol=currentPieceMovedCol;
+    updateCurrentMatrix();
+  }
+}
+
+boolean checkOverlapAndOutOfBoundary(byte pieceIndex) {
+  int row=currentPieceMovedRow;
+  int col=currentPieceMovedCol;
+  for (int i=0;i<4;i++) {
+    for (int j=0; j<4;j++) {
+      if (pieces[pieceIndex][j][i]>0) {   // (row+j>-1 && row+j<rows)
+        if (!( row+j<rows && (col+i>-1 && col+i<cols))) return true;  // checks out of boundary
+        else if (currentMatrix[row+j][col+i]<0) return true;  // checks overlap
+      }
+    }
+  }
+  return false;
+}
+
+void rotate() {
+  if (currentPieceIndex==0) return;
+  else if (currentPieceIndex<3) currentPieceRotatedIndex=3-currentPieceIndex;
+  else if (currentPieceIndex<5) currentPieceRotatedIndex=7-currentPieceIndex;
+  else if (currentPieceIndex<7) currentPieceRotatedIndex=11-currentPieceIndex;
+  else if ((currentPieceIndex % 4)==2) currentPieceRotatedIndex=currentPieceIndex-3;
+  else currentPieceRotatedIndex=currentPieceIndex+1;
+  
+  currentPieceMovedRow=currentPieceRow;
+  currentPieceMovedCol=currentPieceCol;
+    
+  // check if the rotation is valid 
+  if (!checkOverlapAndOutOfBoundary(currentPieceRotatedIndex)) {
+    clearTrace();
+    currentPieceIndex=currentPieceRotatedIndex;
+    for (int i=0;i<4;i++) {
+      for (int j=0;j<4;j++) {
+        currentPiece[j][i]=pieces[currentPieceRotatedIndex][j][i];
+      }
+    }
+    updateCurrentMatrix();
+  }
+}
+
+boolean checkIfLanded() {
+  for (int i=0;i<4;i++) {
+    for (int j=0; j<4;j++) {
+      if ((currentPieceRow+j>-1 && currentPieceRow+j<rows) && (currentPieceCol+i>-1 && currentPieceCol+i<cols)) {
+        if (currentMatrix[currentPieceRow+j][currentPieceCol+i]>0) {
+          if (currentPieceRow+j==bottomLimit) return true;            // if the piece has dropped on the bottom
+          else if (currentMatrix[currentPieceRow+j+1][currentPieceCol+i]<0) return true;  //if there is any deadblock just below the piece
+        }
+      }
+    }
+  }
+  return false;
+}
+
+boolean generatePiece() {
+  byte pieceIndex=random(7);
+  for (int i=0; i<4; i++) {
+    for (int j=0; j<4; j++) {
+      currentPiece[j][i]=piecesGenerated[pieceIndex][j][i];
+    }
+  }
+  for (int i=0; i<4; i++) {
+    for (int j=0; j<2; j++) {
+      if (piecesGenerated[pieceIndex][j+2][i]>0) {
+        if (currentMatrix[j][i+8]<0) return false;
+      }
+    }
+  }
+  for (int i=0; i<4; i++) {
+    for (int j=0; j<2; j++) {
+      if (piecesGenerated[pieceIndex][j+2][i]>0) {
+        currentMatrix[j][i+8]=piecesGenerated[pieceIndex][j+2][i];
+      }
+    }
+  }
+  currentPieceRow = -2;
+  currentPieceCol = 8;
+  switch (pieceIndex) {
+    case 0:
+      currentPieceIndex=0;
+      break;
+    case 1:
+      currentPieceIndex=1;
+      break;
+    case 2:
+      currentPieceIndex=3;
+      break;
+    case 3:
+      currentPieceIndex=5;
+      break;
+    case 4:
+      currentPieceIndex=7;
+      break;
+    case 5:
+      currentPieceIndex=11;
+      break;
+    case 6:
+      currentPieceIndex=15;
+      break;
+  }
+  return true;
+}
+
+void convertToDeadBlock() {
+  for (int i=0;i<cols;i++) {
+    for (int j=0;j<rows;j++) {
+      if (currentMatrix[j][i]>0) {
+        currentMatrix[j][i]=-currentMatrix[j][i];
+      }
+    }
+  }
+}
+
+void checkLinesCleared() {
+  byte lineErased[rows];
+  byte rowCounter=0;
+  for (int j=0;j<rows;j++) {
+    byte colCounter=0;
+    for (int i=0;i<cols;i++) {
+      if (currentMatrix[j][i]==0) {
+        lineErased[j]=0;
+        break;
+      }
+      else colCounter+=1;
+    }
+    if (colCounter==cols) {
+      lineErased[j]=1;
+      rowCounter+=1;
+    }  
+  }
+  
+  if (rowCounter==0) return;  // if no lines are cleared
+  
+  int tempMatrix[rows-rowCounter][cols];
+  byte rowCounter2=0;
+  for (int j=0;j<rows;j++) {
+    if (lineErased[j]==0) {
+      for (int i=0;i<cols;i++) tempMatrix[rowCounter2][i]=currentMatrix[j][i]+0;
+      rowCounter2+=1;
+    }
+  }
+  for (int j=0;j<rowCounter;j++) {
+    for (int i=0;i<cols;i++) {
+      currentMatrix[j][i]=0;
+    }
+  }
+  for (int j=0;j<rows-rowCounter;j++) {
+    for (int i=0;i<cols;i++) {
+      currentMatrix[j+rowCounter][i]=tempMatrix[j][i];
+    }
+  }
+  numberOfLinesCleared+=rowCounter;
+}
+
+void resetTetris() {
+    for (int i=0;i<cols;i++) {
+      for (int j=0; j<rows; j++)
+        currentMatrix[j][i] = 0;
+    }
+    textColor = random(7)+1;
+    numberOfLinesCleared = 0;
+    
+    // reinitilize variables
+    disableMove = false;
+    pieceDropping = false;
+    fastDrop = false;
+}
+
+void checkRestartGame() {
+  if (commands[8]==true) {
+    resetTetris();
+    gameRunning = true;
+    generatePiece(); // Start with a new one
+  }
+}
+
+void checkRotate() {
+  time= millis(); 
+  if ((commands[2]==true || commands[4]==true || commands[5]==true || commands[6]==true || commands[7]==true)
+      && time-lastButtonPressTime>110 && disableMove==false) {
+    rotate();
+    lastButtonPressTime=millis();
+  }
+}
+
+void startDropping() {
+  time = millis();
+  if (commands[3]==true && time-lastButtonPressTime>110 && disableMove==false) {
+    pieceDropping=true;
+    lastButtonPressTime=millis();
+  }
+}
+
+void checkLeft()  {
+  time= millis();
+  if (commands[1]==true && time-lastButtonPressTime>110 && disableMove==false) {
+    move(3);
+    lastButtonPressTime=millis();
+  }
+}
+
+void checkRight() {  
+  time= millis();
+  if (commands[0]==true && time-lastButtonPressTime>110 && disableMove==false) {
+    move(4);
+    lastButtonPressTime=millis();
+  }
+}
+
+void checkDrop() {  
+  time= millis();
+  if (commands[3]==true && time-lastButtonPressTime>110 && disableMove==false) {
+    fastDrop=true;
+    lastButtonPressTime=millis();
+  }
+}
+
+void setupTetris() {
+  setupBluetooth();
+
+  //  random seed for color selection, direction selection, and food+snakehead generation  
+  byte rand1=random(7);
+  rand1=random(7);
+  rand1=random(7);
+  
+  //initialize variables
+  numberOfLinesCleared=0;
+  isGame = true;
+  gameRunning = true;
+  disableMove = false;
+  fastDrop= false;
+  generatePiece();
+   
+  // Storing these constants below in Flash memory
+  const PROGMEM byte pieces[19][4][4] = {O1, Z1,Z2, S1,S2, I1,I2, J1,J2,J3,J4, L1,L2,L3,L4, T1,T2,T3,T4};
+  const PROGMEM byte piecesGenerated[7][4][4] = {O1, Z1, S1, I1, J1, L1, T1};
+
+}
+
+void destroyTetris() {
+  resetTetris();
+  isGame = false;
+}
+
+void loopTetris() {
+  resetBluetoothCommands();
+  updateBluetoothCommands();
+  checkRight();
+  checkLeft();
+  checkRotate();
+  checkDrop();
+  checkRestartGame();
+  
+  displayFrame();
+  currentMoveTime=millis();
+  if (fastDrop==true) {
+    while (!checkIfLanded()) move(2);
+    displayFrame();
+    fastDrop=false;
+  }
+  if (commands[3]==true) pieceDropping=false;
+  int timeInterval;
+  if (!pieceDropping) timeInterval = 800-numberOfLinesCleared*10;
+  else timeInterval= 120;
+  if (!checkIfLanded() && currentMoveTime-lastMoveTime>timeInterval) {
+    move(2);
+    lastMoveTime=millis();
+  }
+  else if (checkIfLanded()) {
+    lastLandedTime=millis();
+    while (currentLandedTime<lastLandedTime+400) {
+      currentLandedTime=millis();
+      displayFrame();
+    }
+    if (!checkIfLanded()) return;
+    else {
+      disableMove = true;
+      convertToDeadBlock();
+      checkLinesCleared();
+      if (generatePiece()==false) {
+        gameOverFunc();
+        //displayTextOverlay(0);
+      }
+      else disableMove=false;
+    }
+  }
+}
+
+void gameOverFunc() {
+  gameRunning = false;
+}
+
+void displayFrame() {
+  byte color;
+  for (int i=0; i<cols; i++) {
+    for (int j=0; j<rows; j++) {
+      if (currentMatrix[j][i]==0) color=0;
+      else if (currentMatrix[j][i]<0) color=-currentMatrix[j][i];
+      else if (currentMatrix[j][i]==129) color=textColor;
+      else color=currentMatrix[j][i];
+      switch (color) {
+        case 0:
+          strip.setPixelColor(calculer(j, i), 0, 0, 0); 
+          break;
+        case 1:
+          strip.setPixelColor(calculer(j, i), 0, 255, 255); 
+          break;
+        case 2:
+          strip.setPixelColor(calculer(j, i), 255, 0, 255); 
+          break;
+        case 3:
+          strip.setPixelColor(calculer(j, i), 255, 255, 0); 
+          break; 
+        case 4:
+          strip.setPixelColor(calculer(j, i), 0, 0, 255); 
+          break; 
+        case 5:
+          strip.setPixelColor(calculer(j, i), 255, 0, 0); 
+          break; 
+        case 6:
+          strip.setPixelColor(calculer(j, i), 0, 255, 0); 
+          break; 
+        case 7:
+          strip.setPixelColor(calculer(j, i), 0, 0, 255); 
+          break;
+        default:
+          strip.setPixelColor(calculer(j, i), 255, 50, 50); 
+      }
+    }
+  }
+  strip.show();
+}
+
+
 /***************************************************************** ENTRY POINT *******************************************************/
 
 void setup() {
@@ -266,6 +766,9 @@ void setupAnimation(int animation) {
   Serial.println(millis());
 #endif
   switch(animation) {
+    case 3:
+      setupTetris();
+      break;
     case 2:
       setupLineaire();
       break;
@@ -280,6 +783,9 @@ void setupAnimation(int animation) {
 
 void loopAnimation(int animation) {
   switch(animation) {
+    case 3:
+      loopTetris();
+      break;
     case 2:
       loopLineaire();
       break;
@@ -298,6 +804,9 @@ void destroyAnimation(int animation) {
   Serial.println(animation);
 #endif
   switch(animation) {
+    case 3:
+      destroyTetris();
+      break;
     case 2:
       destroyLineaire();
       break;
@@ -306,9 +815,10 @@ void destroyAnimation(int animation) {
       break;
     case 0:
       destroyAuHasard();
-    break;  
+      break;  
   }
 }
+
 
 void loop() {
   unsigned long time = millis();
@@ -322,7 +832,8 @@ void loop() {
     while(1);
   }
   
-  if(time > last_animation_switch + DURATION_ANIMATION_MS) {
+  // Changer d'animation quand le jeu est fini si c'est un jeu ou au bout de DURATION_ANIMATION sinon
+  if((isGame == false && (time > last_animation_switch + DURATION_ANIMATION_MS)) || (isGame == true && gameRunning == false)) {
     last_animation_switch = time;
     fadeOut();
     destroyAnimation(current_animation);
