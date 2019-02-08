dcba4089bfe638f4a6deaf9f151f05560246ac50
lucdems <lucdems@gmail.com>
2019-02-08 14:23:14 +0100

    Show the Server Ip Adress

--- desktop/renderer.js
+++ desktop/renderer.js
@@ -2,7 +2,12 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 const { ipcRenderer } = require('electron');
+let ip = require('ip');
 
+// Show IP
+$('#myIp').text(ip.address());
+
+//Events
 ipcRenderer.on('addUser',function(event,arg){
     var nbConnect = $('#nbConnected').text();
     nbConnect++;
