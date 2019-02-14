a15bc466e1696fbca3282a1b217f6e5f94e50655
lucdems <lucdems@gmail.com>
2019-02-14 11:27:22 +0100

    Blockly code generators

--- Workspace Blockly/generatorstub.js
+++ Workspace Blockly/generatorstub.js
@@ -2,7 +2,7 @@ Blockly.JavaScript['turn_off_the_pixel'] = function(block) {
   var value_row = Blockly.JavaScript.valueToCode(block, 'row', Blockly.JavaScript.ORDER_ATOMIC);
   var value_column = Blockly.JavaScript.valueToCode(block, 'column', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+  var code = `switchOffPixel(${value_row},${value_column});`;
   return code;
 };
 
@@ -10,14 +10,14 @@ Blockly.JavaScript['randomly_color_pixel'] = function(block) {
   var value_row = Blockly.JavaScript.valueToCode(block, 'row', Blockly.JavaScript.ORDER_ATOMIC);
   var value_column = Blockly.JavaScript.valueToCode(block, 'column', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+  var code = `setRandomPixel(${value_row},${value_column});`;
   return code;
 };
 
 
 Blockly.JavaScript['turn_off'] = function(block) {
   // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+  var code = 'switchOffAllPixels();';
   return code;
 };
 
@@ -26,9 +26,9 @@ Blockly.JavaScript['color_pixel'] = function(block) {
   var value_column = Blockly.JavaScript.valueToCode(block, 'column', Blockly.JavaScript.ORDER_ATOMIC);
   var value_red = Blockly.JavaScript.valueToCode(block, 'red', Blockly.JavaScript.ORDER_ATOMIC);
   var value_green = Blockly.JavaScript.valueToCode(block, 'green', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_column = Blockly.JavaScript.valueToCode(block, 'column', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_blue = Blockly.JavaScript.valueToCode(block, 'blue', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+  var code = `setPixel(${value_row},${value_column},{r:${value_red},g:${value_green},b:${value_blue}});`;
   return code;
 };
 
@@ -36,7 +36,7 @@ Blockly.JavaScript['sleep'] = function(block) {
   var text_time = block.getFieldValue('time');
   var dropdown_list = block.getFieldValue('list');
   // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+  var code = `sleep(${text_time},${dropdown_list});`;
   return code;
 };
 
@@ -45,6 +45,6 @@ Blockly.JavaScript['color_pixel_select'] = function(block) {
   var value_column = Blockly.JavaScript.valueToCode(block, 'column', Blockly.JavaScript.ORDER_ATOMIC);
   var colour_couleur = block.getFieldValue('couleur');
   // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
+  var code = `setPixel(${value_row},${value_column},${colour_couleur});`;
   return code;
 };
