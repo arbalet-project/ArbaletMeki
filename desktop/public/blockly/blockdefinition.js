bdaa4f872d48f91333ea8754712f675bdf60da8a
william <william4456@gmail.com>
2019-02-18 14:28:25 +0100

    langue

--- desktop/public/blockly/blockdefinition.js
+++ desktop/public/blockly/blockdefinition.js
@@ -20,11 +20,9 @@ Blockly.Blocks['color_pixel'] = {
 };
 */
 
-
-//Colorer le pixel %1 %2 en %3
-  Blockly.defineBlocksWithJsonArray([{
+Blockly.defineBlocksWithJsonArray([{
     "type": "color_pixel",
-    "message0": "Colorer le pixel %1 %2 en %3 ",
+    "message0": "%{BKY_COLOR_PIXEL_TITLE}",
     "args0": [
       {
         "type": "input_value",
@@ -43,11 +41,12 @@ Blockly.Blocks['color_pixel'] = {
     "inputsInline": true,
     "previousStatement": null,
     "nextStatement": null,
-    "colour": 230,
-    "tooltip": "Ajouter une ligne et une colonne, puis la couleur",
+    "colour": 0,
+    "tooltip": "%{BKY_COLOR_PIXEL_TOOLTIP}",
     "helpUrl": ""
   }]);
-
+  
+/*
  Blockly.Blocks['color_all_pixels'] = {
   init: function() {
     this.appendValueInput("color")
@@ -60,7 +59,67 @@ Blockly.Blocks['color_pixel'] = {
  this.setTooltip("");
  this.setHelpUrl("");
   }
-};
+};*/
+Blockly.defineBlocksWithJsonArray([
+{
+  "type": "color_all_pixels",
+  "message0": "%{BKY_COLOR_ALL_PIXELS_TITLE}",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "color",
+      "check": "Colour"
+    }
+  ],
+  "inputsInline": true,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 0,
+  "tooltip": "%{BKY_COLOR_ALL_PIXELS_TOOLTIP}",
+  "helpUrl": ""
+}]);
+
+
+/*
+Blockly.Blocks['turn_off_the_pixel'] = {
+  init: function() {
+    this.appendValueInput("row")
+        .setCheck("Number")
+        .appendField("Eteindre le pixel");
+    this.appendValueInput("column")
+        .setCheck("Number");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(0);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};*/
+
+Blockly.defineBlocksWithJsonArray([
+{
+  "type": "turn_off_the_pixel",
+  "message0": "%{BKY_TURN_OFF_THE_PIXEL_TITLE}",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "row",
+      "check": "Number"
+    },
+    {
+      "type": "input_value",
+      "name": "column",
+      "check": "Number"
+    }
+  ],
+  "inputsInline": true,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 0,
+  "tooltip": "%{BKY_TURN_OFF_THE_PIXEL_TOOLTIP}",
+  "helpUrl": ""
+}]);
 
 /*Blockly.Blocks['turn_off'] = {
     init: function() {
@@ -79,27 +138,12 @@ Blockly.defineBlocksWithJsonArray([{
     "message0": "%{BKY_TURN_OFF_TITLE}",
     "previousStatement": null,
     "nextStatement": null,
-    "colour": 230,
-    "tooltip": "",
+    "colour": 0,
+    "tooltip": "%{BKY_TURN_OFF_TOOLTIP}",
     "helpUrl": ""
   }]);
 
-Blockly.Blocks['turn_off_the_pixel'] = {
-    init: function() {
-      this.appendValueInput("row")
-          .setCheck("Number")
-          .appendField("Eteindre le pixel");
-      this.appendValueInput("column")
-          .setCheck("Number");
-      this.setInputsInline(true);
-      this.setPreviousStatement(true, null);
-      this.setNextStatement(true, null);
-      this.setColour(0);
-   this.setTooltip("");
-   this.setHelpUrl("");
-    }
-  };
-
+/*
   Blockly.Blocks['var_max_column'] = {
     init: function() {
       this.appendDummyInput()
@@ -110,8 +154,21 @@ Blockly.Blocks['turn_off_the_pixel'] = {
    this.setTooltip("");
    this.setHelpUrl("");
     }
-  };
+  };*/
+
+  Blockly.defineBlocksWithJsonArray([
+  {
+    "type": "var_max_column",
+    "message0": "%{BKY_VAR_MAX_COLUMN_TITLE}",
+    "inputsInline": true,
+    "output": "Number",
+    "colour": 0,
+    "tooltip": "%{BKY_VAR_MAX_COLUMN_TOOLTIP}",
+    "helpUrl": ""
+  }]);
   
+
+  /*
   Blockly.Blocks['var_max_line'] = {
     init: function() {
       this.appendDummyInput()
@@ -122,7 +179,18 @@ Blockly.Blocks['turn_off_the_pixel'] = {
    this.setHelpUrl("");
     }
   };
-  
+  */
+ Blockly.defineBlocksWithJsonArray([
+ {
+  "type": "var_max_line",
+  "message0": "%{BKY_VAR_MAX_ROW_TITLE}",
+  "output": "Number",
+  "colour": 0,
+  "tooltip": "%{BKY_VAR_MAX_ROW_TOOLTIP}",
+  "helpUrl": ""
+}]);
+
+/*
   Blockly.Blocks['pixel_color'] = {
     init: function() {
       this.appendValueInput("row")
@@ -132,12 +200,37 @@ Blockly.Blocks['turn_off_the_pixel'] = {
           .setCheck("Number");
       this.setInputsInline(true);
       this.setOutput(true, null);
-      this.setColour(20);
+      this.setColour(0);
    this.setTooltip("");
    this.setHelpUrl("");
     }
   };  
+*/
+Blockly.defineBlocksWithJsonArray([
 
+{
+  "type": "pixel_color",
+  "message0": "%{BKY_PIXEL_COLOR_TITLE}",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "row",
+      "check": "Number"
+    },
+    {
+      "type": "input_value",
+      "name": "column",
+      "check": "Number"
+    }
+  ],
+  "inputsInline": true,
+  "output": null,
+  "colour": 20,
+  "tooltip": "%{BKY_PIXEL_COLOR_TOOLTIP}",
+  "helpUrl": ""
+}]);
+
+/*
 Blockly.Blocks['sleep'] = {
     init: function() {
       this.appendDummyInput()
@@ -152,3 +245,36 @@ Blockly.Blocks['sleep'] = {
    this.setHelpUrl("");
     }
   };
+  */
+ Blockly.defineBlocksWithJsonArray([
+ {
+  "type": "sleep",
+  "message0": "%{BKY_SLEEP_TITLE}",
+  "args0": [
+    {
+      "type": "field_input",
+      "name": "time",
+      "text": "0"
+    },
+    {
+      "type": "field_dropdown",
+      "name": "list",
+      "options": [
+        [
+          "s",
+          "'s'"
+        ],
+        [
+          "ms",
+          "'ms'"
+        ]
+      ]
+    }
+  ],
+  "inputsInline": true,
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 120,
+  "tooltip": "%{BKY_SLEEP_TOOLTIP}",
+  "helpUrl": ""
+}]);
